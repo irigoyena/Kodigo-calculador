4c3a6c1f8fb1718d8efd4ed39391a183494251d2
irigoyena <alem.irigoyen9@gmail.com>
2022-07-10 11:45:13 -0600

    clase Calculadora creada

---
+++ Calculadora/Calculadora.php
@@ -0,0 +1,10 @@
+<?php
+
+class Calculadora {
+
+    public function __construct() {
+        
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+include 'Calculadora/hello.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora de conversiones</title>
+</head>
+<body>
+    <h1>Calculadora de conversiones</h1>
+    <p>Elige una opcion del menu</p>
+    <nav class="menu">
+        <ul>
+            <li><a href="longitud.html">Longitud</a></li>
+            <li><a href="">Masa</a></li>
+            <li><a href="">Volumen</a></li>
+            <li><a href="">Tiempo</a></li>
+            <li><a href="">Moneda</a></li>
+        </ul>
+    
+    </nav>
+    
+    
+</form>
+</body>
+</html>
